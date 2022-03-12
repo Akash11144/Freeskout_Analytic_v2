8a3dbd3bc04510e7ab5d2ef3b63cf4db8278d6f4
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2022-03-12 17:12:47 +0530

    nkk

--- src/backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const a=require('express');
-const b=a.Router();
-const {user}=require('../models');
-
-b.post('/getUser',async(req,res)=>res.send( await user.insertMany([req.body])))
-
-b.get('/getC',async(req,res)=>res.send(await user.find()));
-
-module.exports=b;
