bad13134b30312ec717281d67f500af5853d958b
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2022-03-14 21:44:25 +0530

    added button in dashboard

--- src/components/redirection/index.js
+++ src/components/redirection/index.js
@@ -223,13 +223,13 @@ const userData = async () => {
 function Redir() {
   const [value, setValue] = useState([]);
   useEffect(async () => {
-    console.log(value)
-    await navigator.geolocation.getCurrentPosition(pos => {
-      // setValue({ tS: pos.timestamp, lat: pos.coords.latitude, longi: pos.coords.longitude })
-      setValue(pos);
-      console.log(pos)
-    },()=>setValue({ tS:'error', lat: 'error', longi:'error'}))
-    console.log(value)
+    // console.log(value)
+    // await navigator.geolocation.getCurrentPosition(pos => {
+    //   // setValue({ tS: pos.timestamp, lat: pos.coords.latitude, longi: pos.coords.longitude })
+    //   setValue(pos);
+    //   console.log(pos)
+    // },()=>setValue({ tS:'error', lat: 'error', longi:'error'}))
+    // console.log(value)
     reDirection();
   }, [])
   return (
