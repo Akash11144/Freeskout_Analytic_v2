7f3458ae4b7eafb0dad0e264c6ebba6a2f86a698
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2022-04-06 18:31:42 +0530

    ihhidca

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router";
+import { Route, Routes, useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import Dash from "../dashboard";
 import GraphDashboard from "../graph-dashboard";
 import Redir from "../redirection";
 import Trial from "../trial";
 import { postR } from "../utlis";
-import { AnimatePresence } from "framer-motion";
 
 let localLink = "http://localhost:8000";
 let mainLink = "https://freeskout-analytic-v2-backend.herokuapp.com";
@@ -47,69 +46,67 @@ const MainRoutes = () => {
   };
 
   return (
-    <AnimatePresence>
-      <Routes location={loc} key={loc.pathname}>
-        <Route path="/rd" element={<Redir></Redir>}></Route>
-        <Route path="/da" element={<Dash></Dash>}></Route>
-        <Route path="/tr" element={<Trial></Trial>}></Route>
-        <Route path="/gda" element={<GraphDashboard></GraphDashboard>}></Route>
-        <Route
-          path="/"
-          element={
-            <div className="container mx-auto">
-              <h1>Hello Home</h1>
-              <Link className="btn btn-outline-warning btn-lg m-1" to="/rd">
-                Freeskout
-              </Link>
-              <Link className="btn btn-outline-info btn-lg m-1" to="/da">
-                user dashboard
-              </Link>
-              <Link className="btn btn-outline-primary btn-lg m-1" to="/gda">
-                Graph dashboard
-              </Link>
-              <div className="homeFormCont">
-                <form>
-                  <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                    />
-                    <div id="emailHelp" className="form-text">
-                      We'll never share your email with anyone else.
-                    </div>
+    <Routes>
+      <Route path="/rd" element={<Redir></Redir>}></Route>
+      <Route path="/da" element={<Dash></Dash>}></Route>
+      <Route path="/tr" element={<Trial></Trial>}></Route>
+      <Route path="/gda" element={<GraphDashboard></GraphDashboard>}></Route>
+      <Route
+        path="/"
+        element={
+          <div className="container mx-auto">
+            <h1>Hello Home</h1>
+            <Link className="btn btn-outline-warning btn-lg m-1" to="/rd">
+              Freeskout
+            </Link>
+            <Link className="btn btn-outline-info btn-lg m-1" to="/da">
+              user dashboard
+            </Link>
+            <Link className="btn btn-outline-primary btn-lg m-1" to="/gda">
+              Graph dashboard
+            </Link>
+            <div className="homeFormCont">
+              <form>
+                <div className="mb-3">
+                  <label for="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  <div id="emailHelp" className="form-text">
+                    We'll never share your email with anyone else.
                   </div>
-                  <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="exampleInputPassword1"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="btn btn-primary"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleLogin();
-                    }}
-                  >
-                    Submit
-                  </button>
-                </form>
-              </div>
+                </div>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleLogin();
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-          }
-        ></Route>
-      </Routes>
-    </AnimatePresence>
+          </div>
+        }
+      ></Route>
+    </Routes>
   );
 };
 
