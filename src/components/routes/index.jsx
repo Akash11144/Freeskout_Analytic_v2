9d350b6250d5f0e3ac46cf73152dab900ecdc0ce
Shubham Upadhay <Shubham.0@outlook.com>
2022-04-26 11:07:28 +0530

    l

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -6,6 +6,8 @@ import GraphDashboard from "../graph-dashboard";
 import Redir from "../redirection";
 import Trial from "../trial";
 import { postR } from "../utlis";
+import Styles from "../routes/index.module.css";
+import companyLogo from "../routes/freeskout.png";
 
 let localLink = "http://localhost:8000";
 let mainLink = "https://freeskout-analytic-v2-backend.herokuapp.com";
@@ -53,54 +55,94 @@ const MainRoutes = () => {
       <Route
         path="/"
         element={
-          <div className="container mx-auto">
-            <h1>Hello Home</h1>
-            <Link className="btn btn-outline-warning btn-lg m-1" to="/rd">
-              Freeskout
-            </Link>
-            <Link className="btn btn-outline-info btn-lg m-1" to="/da">
-              user dashboard
-            </Link>
-            <Link className="btn btn-outline-primary btn-lg m-1" to="/gda">
-              Graph dashboard
-            </Link>
-            <div className="homeFormCont">
-              <form>
-                <div className="mb-3">
-                  <label for="exampleInputEmail1" className="form-label">
-                    Email address
-                  </label>
+          // <div className="container mx-auto">
+          //   <h1>Hello Home</h1>
+          //   <Link className="btn btn-outline-warning btn-lg m-1" to="/rd">
+          //     Freeskout
+          //   </Link>
+          //   <Link className="btn btn-outline-info btn-lg m-1" to="/da">
+          //     user dashboard
+          //   </Link>
+          //   <Link className="btn btn-outline-primary btn-lg m-1" to="/gda">
+          //     Graph dashboard
+          //   </Link>
+          //   <div className="homeFormCont">
+          //     <form>
+          //       <div className="mb-3">
+          //         <label for="exampleInputEmail1" className="form-label">
+          //           Email address
+          //         </label>
+          //         <input
+          //           type="email"
+          //           className="form-control"
+          //           id="exampleInputEmail1"
+          //           aria-describedby="emailHelp"
+          //         />
+          //         <div id="emailHelp" className="form-text">
+          //           We'll never share your email with anyone else.
+          //         </div>
+          //       </div>
+          //       <div className="mb-3">
+          //         <label for="exampleInputPassword1" className="form-label">
+          //           Password
+          //         </label>
+          //         <input
+          //           type="password"
+          //           className="form-control"
+          //           id="exampleInputPassword1"
+          //         />
+          //       </div>
+          //       <button
+          //         type="submit"
+          //         className="btn btn-primary"
+          //         onClick={(e) => {
+          //           e.preventDefault();
+          //           handleLogin();
+          //         }}
+          //       >
+          //         Submit
+          //       </button>
+          //     </form>
+          //   </div>
+          // </div>
+          <div className={Styles.maindiv}>
+            <div className={Styles.Glassdiv}></div>
+            <div className={Styles.loginMainDiv}>
+              <div className={Styles.FsImgCont}>
+                <img src={companyLogo} alt="Company Logo" />
+              </div>
+              <div className={Styles.formContainer}>
+                <div className={Styles.form__group}>
                   <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
+                    type="text"
+                    class={Styles.form__input}
+                    id="name"
+                    placeholder="Username"
+                    required=""
                   />
-                  <div id="emailHelp" className="form-text">
-                    We'll never share your email with anyone else.
-                  </div>
-                </div>
-                <div className="mb-3">
-                  <label for="exampleInputPassword1" className="form-label">
-                    Password
+                  <label for="name" class={Styles.form__label}>
+                    Username
                   </label>
+                </div>
+                <div className={Styles.form__group}>
                   <input
                     type="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
+                    class={Styles.form__input}
+                    id="password"
+                    placeholder="Secret"
+                    required=""
                   />
+                  <label for="name" class={Styles.form__label}>
+                    Secret
+                  </label>
                 </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleLogin();
-                  }}
-                >
-                  Submit
-                </button>
-              </form>
+              </div>
+              <div className={Styles.loginBtnCont}>
+                <div className={Styles.logInBtn}>Log Me In</div>
+                <Link className={Styles.siteLinkBtn} to="">
+                  www.freeskout.com
+                </Link>
+              </div>
             </div>
           </div>
         }
