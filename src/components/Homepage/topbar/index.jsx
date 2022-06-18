406d6a1ccac6f418f3cac2aa76924a7a4e6a13fd
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2022-06-18 23:09:04 +0530

    l

--- src/components/Homepage/topbar/index.jsx
+++ src/components/Homepage/topbar/index.jsx
@@ -11,20 +11,20 @@ const hamClick = () => {
   var a = topLine.classList;
   if (a.length == 1) {
     topLine.classList.add("temp");
-    topLine.style.transform="translateY(13px) rotate(45deg)"
-    middleLine.style.opacity='0'
-    middleLine.style.transform='translateX(16px)'
-    BottomLine.style.transform='translateY(-11px) rotate(-45deg)'
-    sidebar.style.opacity='1';
-    sidebar.style.top='10vh'
+    topLine.style.transform = "translateY(13px) rotate(45deg)";
+    middleLine.style.opacity = "0";
+    middleLine.style.transform = "translateX(16px)";
+    BottomLine.style.transform = "translateY(-11px) rotate(-45deg)";
+    sidebar.style.opacity = "1";
+    sidebar.style.top = "10vh";
   } else {
     topLine.classList.remove("temp");
-    topLine.style.transform="unset"
-    middleLine.style.opacity='1'
-    middleLine.style.transform='unset'
-    BottomLine.style.transform='unset'
-    sidebar.style.opacity='0';
-    sidebar.style.top='-80vh'
+    topLine.style.transform = "unset";
+    middleLine.style.opacity = "1";
+    middleLine.style.transform = "unset";
+    BottomLine.style.transform = "unset";
+    sidebar.style.opacity = "0";
+    sidebar.style.top = "-100vh";
   }
 };
 
@@ -33,14 +33,14 @@ const topbar = (props) => {
     <div className={Styles.allCont}>
       {console.log("topbar props", props)}
       <div className={Styles.mainCont}>
-        <div className={Styles.leftMain}>
-          <div
-            className={Styles.hamCont}
-            id="hamburger"
-            onClick={() => {
-              hamClick();
-            }}
-          >
+        <div
+          className={Styles.leftMain}
+          id="hamburger"
+          onClick={() => {
+            hamClick();
+          }}
+        >
+          <div className={Styles.hamCont}>
             <span className={`${Styles.line}`} id="topLine"></span>
             <span className={`${Styles.line}`} id="bottomLine"></span>
             <span className={`${Styles.line}`} id="endLine"></span>
