a78d7df865443489b458f0fa2a5295b69df079a6
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2022-06-28 11:43:41 +0530

    l

--- src/components/Homepage/linkManagement/index.jsx
+++ src/components/Homepage/linkManagement/index.jsx
@@ -1,5 +1,53 @@
+import React from "react";
+import Styles from "../linkManagement/index.module.css";
+const getYear = () => {
+  var dt = new Date();
+  return dt.getFullYear();
+};
 const LinkManement = () => {
-  return <div>heloo Welcome to Link Management</div>;
+  return (
+    <div className={Styles.mainCont}>
+      {/* {generateLoading && <SmallLoading />} */}
+      <div className={Styles.secondaryDiv}>
+        <div className={Styles.selectors}>
+          <div className={Styles.togggleBtnHolder}>
+            <div className={Styles.switches_container}>
+              <input
+                type="radio"
+                id="switchActive"
+                name="switchBtn"
+                value="active"
+                checked="checked"
+              />
+              <input
+                type="radio"
+                id="switchDeleted"
+                name="switchBtn"
+                value="deleted"
+              />
+              <label for="switchMonthly">Monthly</label>
+              <label for="switchYearly">Yearly</label>
+              <div className={Styles.switch_wrapper}>
+                <div className={Styles.switch}>
+                  <div>Monthly</div>
+                  <div>Yearly</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={Styles.linkList}></div>
+      </div>
+      <div className={Styles.copyRightCont}>
+        <p className={Styles.CopyRightP}>
+          © Freeskout{" "}
+          <span className={Styles.CRyearCont} id="CRyear">
+            {getYear()}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LinkManement;
