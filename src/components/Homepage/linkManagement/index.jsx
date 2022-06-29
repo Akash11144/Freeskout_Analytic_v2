4f54cc8727e40a0aa9d3852c64b3f1a0be082d57
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2022-06-29 15:03:25 +0530

    l

--- src/components/Homepage/linkManagement/index.jsx
+++ src/components/Homepage/linkManagement/index.jsx
@@ -1,16 +1,39 @@
 import { useState, useEffect } from "react";
 import Styles from "../linkManagement/index.module.css";
 import { AiFillCaretDown } from "react-icons/ai";
+import { useRef } from "react";
 const getYear = () => {
   var dt = new Date();
   return dt.getFullYear();
 };
-const LinkManement = () => {
+const users = [{ user: "A" }, { user: "B" }, { user: "C" }];
+const LinkManement = (props) => {
   const [isActive, setisActive] = useState(false);
+  const [isUserActive, setisUserActive] = useState(false);
   const [Data, setData] = useState([]);
   const handleStatusSelector = () => {
     setisActive(!isActive);
   };
+  const handleUserSelector = () => {
+    setisUserActive(!isUserActive);
+  };
+  const selected_status = useRef(null);
+  const all_links = useRef(null);
+  const active_links = useRef(null);
+  const deleted_links = useRef(null);
+  const allClick = () => {
+    let allLiIt = all_links.current.innerText;
+    selected_status.current.innerText = allLiIt;
+  };
+  const activeClick = () => {
+    let alIt = active_links.current.innerText;
+    selected_status.current.innerText = alIt;
+  };
+  const deletedClick = () => {
+    let dlIt = deleted_links.current.innerText;
+    selected_status.current.innerText = dlIt;
+  };
+  const selected_user = useRef();
   let i = false;
   useEffect(() => {
     if (!i) {
@@ -33,7 +56,7 @@ const LinkManement = () => {
       <div className={Styles.secondaryDiv}>
         <div className={Styles.selectors}>
           <div className={Styles.selectedOption}>
-            <p>All Links</p>
+            <p ref={selected_status}>All Links</p>
             <div
               className={Styles.dropholder}
               onClick={() => {
@@ -57,29 +80,73 @@ const LinkManement = () => {
                 className={Styles.otherOptions}
                 onClick={() => {
                   handleStatusSelector();
+                  allClick();
                 }}
               >
-                <p>All Links</p>
+                <p ref={all_links}>All Links</p>
               </div>
               <div
                 className={Styles.otherOptions}
                 onClick={() => {
                   handleStatusSelector();
+                  activeClick();
                 }}
               >
-                <p>Active Links</p>
+                <p ref={active_links}>Active Links</p>
               </div>
               <div
                 className={Styles.otherOptions}
                 onClick={() => {
                   handleStatusSelector();
+                  deletedClick();
                 }}
               >
-                <p>Deleted Links</p>
+                <p ref={deleted_links}>Deleted Links</p>
+              </div>
+            </div>
+          </div>
+          <div className={Styles.selectUser}>
+            <div className={Styles.selectedUserOption}>
+              <p ref={selected_user}>All Users</p>
+              <div
+                className={Styles.dropholder}
+                onClick={() => {
+                  // handleStatusSelector();
+                  handleUserSelector();
+                }}
+              >
+                <AiFillCaretDown
+                  className={`${Styles.downIcon}
+              ${isUserActive ? Styles.rotatedIcon : Styles.downIcon}`}
+                />
+              </div>
+              <div
+                className={`${Styles.otherOptionsContShow}
+            ${
+              isUserActive
+                ? Styles.otherOptionsContShow
+                : Styles.otherOptionsContHide
+            }`}
+              >
+                {/* <Users searchV={(d) => handleUserSelector()}></Users> */}
+                {users.length &&
+                  users.map((item, index) => {
+                    return (
+                      <div
+                        className={Styles.otherOptions}
+                        onClick={() => {
+                          handleUserSelector();
+                        }}
+                      >
+                        <p>{item.user}</p>
+                      </div>
+                    );
+                  })}
               </div>
             </div>
           </div>
         </div>
+
         <div className={Styles.linkList}></div>
       </div>
       <div className={Styles.copyRightCont}>
