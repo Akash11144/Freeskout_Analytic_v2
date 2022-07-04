13391df6c359222d642d331e6c5e53f757854c1a
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2022-07-04 11:05:58 +0530

    dudh

--- src/components/Redirection/redirecting-routes/index.jsx
+++ src/components/Redirection/redirecting-routes/index.jsx
@@ -1,10 +1,11 @@
-import { useRoutes, Outlet } from "react-router-dom";
+import { useRoutes, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Redir from "../redirection-page";
 import PageNotFound from "../page-not-found";
 
 function RedirectingRoutes() {
   const [Data, setData] = useState([]);
+  const loc = useLocation();
   let arr = [
     {
       path: "/",
@@ -21,10 +22,13 @@ function RedirectingRoutes() {
   ];
   let i = false;
   useEffect(() => {
+    console.log("location", loc);
     if (!i) {
       const getData = async () => {
         let r = await fetch("http://localhost:1111/route/allRoutesRedis");
         let r1 = await r.json();
+        const newloc = loc.pathname.split("/");
+        console.log(newloc);
         for (let i = 0; i < r1.length; i++)
           arr.push({ path: r1[i].path, element: <Redir {...r1} /> });
         setData(arr);
