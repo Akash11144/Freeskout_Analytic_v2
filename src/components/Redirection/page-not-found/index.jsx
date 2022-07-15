4c7fe608a30b2e9786e6546eaaca619e72b253a5
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2022-07-15 13:13:02 +0530

    l

--- src/components/Redirection/page-not-found/index.jsx
+++ src/components/Redirection/page-not-found/index.jsx
@@ -1,8 +1,29 @@
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import Styles from "./index.module.css";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
 const PageNotFound = () => {
+  const [inApperror, setinApperror] = useState(false);
+  const [newUrl, setnewUrl] = useState("");
+  let loc = useLocation();
+  useEffect(() => {
+    let currentLoc = loc.pathname.split("/");
+    console.log(currentLoc[1]);
+    if (currentLoc[1] === "redirect") {
+      setnewUrl("http://localhost:3000/redirect/freeskout");
+    } else if (currentLoc[1] === "home") {
+      setinApperror(true);
+      setnewUrl("http://localhost:3000/home/");
+    } else {
+      setnewUrl("/");
+    }
+  }, []);
   return (
-    <div className={Styles.mainDiv}>
+    <div
+      className={`${Styles.mainDiv} ${
+        inApperror ? Styles.mainDivApp : Styles.mainDiv
+      }`}
+    >
       <div className={Styles.gifCont}>
         <h1 className={Styles.ECholder}>404</h1>
       </div>
@@ -14,7 +35,7 @@ const PageNotFound = () => {
         <div
           className={Styles.homeBtn}
           onClick={() => {
-            window.open("http://localhost:3000/redirect/freeskout", "_self");
+            window.open(newUrl, "_self");
           }}
         >
           Take me Home
