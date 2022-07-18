0fd2ce46f8dce206204047a248f39eb431e3e96a
Shubham Upadhay <82693486+ShubhamUpadhay@users.noreply.github.com>
2022-07-18 12:21:03 +0530

    l

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -69,6 +69,7 @@ const Home = () => {
               <Topbar {...person}></Topbar>
             </div>
             <HomeRoutes {...person} />
+            <NoInternet></NoInternet>
           </>
         ) : (
           pageError && <InformationPopUp {...errorObj} />
